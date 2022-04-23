c64158ca84d6901b1b4112a8146430e90ad992f7
idrissa-gif <idrissa@iut-dhaka.edu>
2022-04-24 03:57:05 +0600

    Initial Commit

---
+++ Icalories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Icalories
+//
+//  Created by idrissa Mahamoudou Dicko on 24/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Icalories/IcaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcaloriesApp.swift
+//  Icalories
+//
+//  Created by idrissa Mahamoudou Dicko on 24/4/22.
+//
+
+import SwiftUI
+
+@main
+struct IcaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
