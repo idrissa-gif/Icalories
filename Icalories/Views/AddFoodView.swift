bddb3d556326f7e187f0e48ee405e045d069b1b3
idrissa-gif <idrissa@iut-dhaka.edu>
2022-04-24 15:10:09 +0600

    done with the database connection

---
+++ Icalories/Views/AddFoodView.swift
@@ -0,0 +1,20 @@
+//
+//  AddFoodView.swift
+//  Icalories
+//
+//  Created by idrissa Mahamoudou Dicko on 24/4/22.
+//
+
+import SwiftUI
+
+struct AddFoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddFoodView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFoodView()
+    }
+}
